643626cda33ee14955a9dba9b34c165a122f2418
Subu89 <sudipmhnt@gmail.com>
2023-01-09 18:52:18 +0530

    last commit

--- src/components/CityAQI/CityAQIDetails.js
+++ src/components/CityAQI/CityAQIDetails.js
@@ -1,94 +1,106 @@
-import React from 'react';
-import { useAQIAPIs } from '../../utils/useAQIAPIs';
+import React from "react";
+import { useAQIAPIs } from "../../utils/useAQIAPIs";
+import CityAQIForecast from "./CityAQIForecast";
 
-import { TOKEN, FEED_AQI_BASE_URL } from '../../utils/AQIConst';
-import { Card, ListGroup } from 'react-bootstrap';
+import { TOKEN, FEED_AQI_BASE_URL } from "../../utils/AQIConst";
+import { Card, ListGroup, Container, Row, Col } from "react-bootstrap";
 
 const CityAQIDetails = (props) => {
-    const [info, error] = useAQIAPIs(
-        `${FEED_AQI_BASE_URL}@${props.uid}/?token=${TOKEN}`
-    );
+  const [info, error] = useAQIAPIs(
+    `${FEED_AQI_BASE_URL}@${props.uid}/?token=${TOKEN}`
+  );
 
-    const names = {
-        'pm25': "particular matter 2.5(pm 2.5)",
-        'pm10': "particular matter 10(pm 10)",
-        'o3': "Ozone",
-        'no2': "Nitrogen Dioxide",
-        'so2': "Sulphur Dioxide",
-        'co': "Carbon Monoxide",
-        't': "Temperature",
-        'w': "Wind",
-        'r': "Rain (precipitation)",
-        'h': "Relative Humidity",
-        'd': "Dew",
-        'p': "Atmospheric Pressure"
+  const names = {
+    pm25: "particular matter 2.5(pm 2.5)",
+    pm10: "particular matter 10(pm 10)",
+    o3: "Ozone",
+    no2: "Nitrogen Dioxide",
+    so2: "Sulphur Dioxide",
+    co: "Carbon Monoxide",
+    t: "Temperature",
+    w: "Wind",
+    r: "Rain (precipitation)",
+    h: "Relative Humidity",
+    d: "Dew",
+    p: "Atmospheric Pressure",
+  };
+
+  const getSpectrum = (iaqi) => {
+    let ret = [];
+    Object.entries(iaqi).map(function (item) {
+      let obj = {};
+      let key = names[item[0]] ? names[item[0]] : item[0];
+      obj["key"] = key;
+      obj["value"] = item[1].v;
+      ret.push(obj);
+    });
+    return ret;
+  };
+
+  const colorize = (name, value) => {
+    if (
+      [
+        "particular matter 2.5(pm 2.5)",
+        "particular matter 10(pm 10)",
+        "Ozone",
+        "Nitrogen Dioxide",
+        "Sulphur Dioxide",
+        "Carbon Monoxide",
+      ].indexOf(name) < 0
+    ) {
+      return "";
     }
-
-    const getSpectrum = (iaqi) => {
-        let ret = [];
-        Object.entries(iaqi).map(function(item) {
-            let obj = {};
-            let key = names[item[0]] ? names[item[0]] : item[0];
-            obj['key'] = key;
-            obj['value'] = item[1].v;
-            ret.push(obj);
-        });
-        return ret;
+    if (value >= 0 && value <= 50) {
+      return "good";
+    } else if (value >= 51 && value <= 100) {
+      return "moderate";
+    } else if (value >= 101 && value <= 150) {
+      return "unhealthy sensitive";
+    } else if (value >= 151 && value <= 200) {
+      return "unhealthy";
+    } else if (value >= 201 && value <= 300) {
+      return "very unhealthy";
+    } else if (value >= 301) {
+      return "hazardous";
     }
+  };
 
-    const colorize = (name, value) => {
-        if ([
-            'particular matter 2.5(pm 2.5)',
-            'particular matter 10(pm 10)',
-            'Ozone',
-            'Nitrogen Dioxide',
-            'Sulphur Dioxide',
-            'Carbon Monoxide'
-        ].indexOf(name) < 0) {
-            return '';
-        }
-        if (value >= 0 && value <= 50) {
-            return 'good';
-        }
-        else if (value >= 51 && value <= 100) {
-            return 'moderate';
-        }
-        else if (value >= 101 && value <= 150) {
-            return 'unhealthy sensitive';
-        }
-        else if (value >= 151 && value <= 200) {
-            return 'unhealthy';
-        }
-        else if (value >= 201 && value <= 300) {
-            return 'very unhealthy';
-        }
-        else if (value >= 301) {
-            return 'hazardous';
-        }
-    }
-
-    return (
-        <React.Fragment>
-            {error}
-            {
-                info.data ?
-                    <Card className="details" style={{ width: '18rem' }}>
-                        <Card.Header>Prominent Pollutant is, <b>{names[info.data.dominentpol]}</b></Card.Header>
-                        <ListGroup variant='flush' className='my-4'>
-                            {
-                                getSpectrum(info.data.iaqi).map((spectrum, i) => (
-                                    <ListGroup.Item key={i}>
-                                        <span className={`dot ${colorize(spectrum.key, spectrum.value)}`}></span>
-                                        <span>{spectrum.key}</span>: <span>{spectrum.value}</span>
-                                    </ListGroup.Item>
-                                ))
-                            }
-                        </ListGroup>
-                    </Card>
-                    : <span>Loading...</span>
-            }
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      {error}
+      {info.data ? (
+        <Container>
+          <Row>
+            <Col>
+              <Card className="details" style={{ width: "18rem" }}>
+                <Card.Header>
+                  Prominent Pollutant is, <b>{names[info.data.dominentpol]}</b>
+                </Card.Header>
+                <ListGroup variant="flush" className="my-4">
+                  {getSpectrum(info.data.iaqi).map((spectrum, i) => (
+                    <ListGroup.Item key={i}>
+                      <span
+                        className={`dot ${colorize(
+                          spectrum.key,
+                          spectrum.value
+                        )}`}
+                      ></span>
+                      <span>{spectrum.key}</span>: <span>{spectrum.value}</span>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </Card>
+            </Col>
+            <Col>
+              <CityAQIForecast forecast={info.data.forecast.daily} />
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        <span>Loading...</span>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default CityAQIDetails;
--- src/components/CityAQI/CityAQIForecast.js
+++ src/components/CityAQI/CityAQIForecast.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Card } from "react-bootstrap";
 
 const CityAQIForecast = (props) => {
   let yesterdayDate = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
@@ -14,8 +18,60 @@ const CityAQIForecast = (props) => {
   let formattedYesterday = convertDate(yesterdayDate);
   let formattedToday = convertDate(todayDate);
   let formattedTomorrowDate = convertDate(tomorrowDate);
+  let forecastData = props.forecast;
+  console.log(formattedToday, formattedTomorrowDate, formattedYesterday);
 
-  return <div>CityAQIForecast</div>;
+  function checkForecast(item) {
+    if (item.day === formattedToday) {
+      return true;
+    } else if (item.day === formattedTomorrowDate) {
+      return true;
+    } else if (item.day === formattedYesterday) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  const getForecastValues = (forecast) => {
+    let ret = [];
+    Object.entries(forecast).map(function (item) {
+      let obj = {};
+      let key = item[0];
+      obj["key"] = key;
+      obj["value"] = forecast[item[0]].filter(checkForecast);
+      ret.push(obj);
+    });
+    return ret;
+  };
+
+  return [formattedYesterday, formattedToday, formattedTomorrowDate].map(
+    (date) => (
+      <div>
+        <h3>{date}</h3>
+        {getForecastValues(forecastData).map((item) => (
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>{item.key}</Card.Title>
+              <Card.Text>
+                {item.value
+                  .filter(function (val) {
+                    return val.day === date;
+                  })
+                  .map((value) => (
+                    <>
+                      <p>Average: {value.avg}</p>
+                      <p>Maximum: {value.max}</p>
+                      <p>Minimum: {value.min}</p>
+                    </>
+                  ))}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    )
+  );
 };
 
 export default CityAQIForecast;
